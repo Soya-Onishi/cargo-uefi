6d48649cb7b14a9a5218ebb9f725335ce7b1656d
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-11-17 23:12:10 +0900

    Cargo.tomlからバイナリの名前の候補を収集する機能を追加

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,31 @@ use std::env;
 use std::path;
 use std::process::ExitStatus;
 use clap::Parser;
+use toml_edit::easy;
+use toml_edit::easy::value::Value;
+use serde::Deserialize;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(long, value_name = "FILE")]
     bin: String
+}
+
+#[derive(Deserialize)]
+struct TomlConfig {
+    package: TomlPackage,
+    bin: Option<Vec<TomlBin>>
+}
+
+#[derive(Deserialize)]
+struct TomlPackage {
+    name: Option<String>
+}
+
+#[derive(Deserialize)]
+struct TomlBin {
+    name: Option<String>
 }
 
 fn main() -> Result<(), io::Error> {
@@ -22,20 +41,16 @@ fn main() -> Result<(), io::Error> {
     let app_path = get_uefi_app(project_root, app_name.as_str())?;
 
     // UEFIアプリケーションを配置するための一時ディレクトリを作成
-    let mut uefi_app_dir = env::temp_dir();
-    uefi_app_dir.push("UEFI");
-    let uefi_root = uefi_app_dir.clone();
-    let uefi_root = uefi_root.as_path();
-    uefi_app_dir.push("EFI");
-    uefi_app_dir.push("BOOT"); 
+    let uefi_root = env::temp_dir().join("UEFI");
+    let uefi_app_dir = env::temp_dir().join("UEFI").join("EFI").join("BOOT");
     std::fs::create_dir_all(uefi_app_dir.as_path())?;
 
     // 作成したディレクトリにUEFIアプリケーションを配置
-    uefi_app_dir.push("BOOTX64.EFI");
-    std::fs::copy(app_path, uefi_app_dir)?;
+    let uefi_app_path = uefi_app_dir.join("BOOTX64.EFI");  
+    std::fs::copy(app_path, uefi_app_path)?;
 
     // QEMUを実行
-    run_qemu(qemu_path.as_path(), ovmf_path.as_path(), uefi_root)?;
+    run_qemu(qemu_path.as_path(), ovmf_path.as_path(), uefi_root.as_path())?;
 
     Ok(())
 }
@@ -104,4 +119,117 @@ fn run_qemu(qemu: &path::Path, ovmf: &path::Path, uefi_root: &path::Path) -> Res
         .spawn()?;
 
    process.wait()
+}
+
+fn get_binary_name(toml: &str) -> Result<Vec<String>, toml_edit::de::Error> {
+    let toml = easy::from_str::<TomlConfig>(toml)?;
+    let bin_names: Vec<String> = toml.bin
+        .unwrap_or(Vec::new())
+        .into_iter()
+        .filter_map(|b| b.name)
+        .collect();
+    let names = if bin_names.is_empty() {
+        toml.package.name.into_iter().collect()
+    } else {
+        bin_names
+    };
+    
+    Ok(names)
+}
+
+fn get_binary_names(toml: &Value) -> Vec<&String> {
+    fn get_from_bins(toml: &Value) -> Vec<&String> {
+        let value = toml.as_table()
+            .map(|t| t.get("bin"))
+            .flatten();
+
+        match value {
+            Some(Value::Array(array)) => array.iter()
+                .filter_map(|elem| elem.as_table())
+                .filter_map(|m| m.get("name"))
+                .filter_map(|name| if let Value::String(s) = name { Some(s) } else { None })
+                .collect(),
+            Some(Value::Table(table)) => table.get("name")
+                .into_iter()
+                .filter_map(|v| if let Value::String(s) = v { Some(s) } else { None })
+                .collect(),
+            _ => Vec::new(),
+        }
+    }
+
+    fn get_from_package(toml: &Value) -> Option<&String> {
+        toml.as_table()
+            .map(|t| t.get("package"))
+            .flatten()
+            .map(|t| t.get("name"))
+            .flatten()
+            .map(|n| if let Value::String(s) = n { Some(s) } else { None })
+            .flatten()
+    }
+    
+    let bin_names = get_from_bins(toml);
+    if bin_names.is_empty() {
+        get_from_package(toml).into_iter().collect()
+    } else {
+        bin_names
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use toml_edit::easy;
+
+    use crate::get_binary_name;
+
+    #[test]
+    fn parse_one_bin_pattern() {
+        let toml = r#"
+        [package] 
+        name = "hoge"
+        
+        [[bin]]
+        name = "fuga"
+        path = "src/fuga/main.rs"
+        "#;
+        
+        let names = get_binary_name(toml).unwrap();
+        assert_eq!(names.len(), 1);
+        assert_eq!(names[0], "fuga");
+    }
+
+    #[test]
+    fn parse_multiple_bin_pattern() {
+        let toml = r#"
+        [package]
+        name = "hoge" 
+
+        [[bin]]
+        name = "hogehoge"
+        path = "src/hogehoge/main.rs"
+
+        [[bin]]
+        name = "fugafuga"
+        path = "src/fugafuga/main.rs" 
+        "#;
+
+        let names = get_binary_name(toml).unwrap();
+        assert_eq!(names.len(), 2);
+        assert_eq!(names[0], "hogehoge");
+        assert_eq!(names[1], "fugafuga"); 
+    }
+
+    #[test]
+    fn parse_package_pattern() {
+        let toml = r#"
+        [package]
+        name = "hoge"
+        
+        [dependencies]
+        fuga = "0.1.0"
+        "#;
+
+        let names = get_binary_name(toml).unwrap();
+        assert_eq!(names.len(), 1);
+        assert_eq!(names[0], "hoge");
+    }
 }
